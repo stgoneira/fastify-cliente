599eff0dcb791061292c4123a425d689f4caa775
stgoneira <stgoneiran@gmail.com>
2023-04-25 20:15:56 -0400

    guardar categoria con React

---
+++ admin/components/categoria/form.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+export default function CategoriaForm() {
+
+    const [nombre, setNombre]           = useState('');
+    const [descripcion, setDescripcion] = useState('');
+
+    const procesarFormulario = async (eventoSubmit) => {
+        try {
+            eventoSubmit.preventDefault();
+            const categoria = {
+                nombre,
+                descripcion
+            };
+
+            const baseUrl   = 'http://localhost:3000';
+            const url       = baseUrl + '/categoria';
+
+            const respuesta = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify( categoria )
+            });
+
+            if( !respuesta.ok ) throw new Error("No se pudo guardar la categoría...");
+
+            const categoriaGuardada = await respuesta.json();
+            console.dir( categoriaGuardada );
+        } catch (error) {
+            console.error( error );
+        }
+    };
+
+    return (
+        <>
+            <form action="form" method="post" onSubmit={procesarFormulario}>
+                <label htmlFor="nombre">Nombre</label>
+                <input type="text" id="nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+
+                <label>Descripción</label>
+                <textarea value={descripcion} onChange={(e) => setDescripcion(e.target.value)}></textarea>
+
+                <button type="submit">Guardar</button>
+            </form>
+
+            Nombre: {nombre} | Descripción: {descripcion}
+        </>
+    );
+}
---
+++ admin/pages/categoria.js
@@ -0,0 +1,11 @@
+import CategoriaForm from "@/components/categoria/form";
+
+export default function PaginaCategoria() {
+    return (
+        <>
+            <h1 className="titulo">Categorías</h1>
+
+            <CategoriaForm />
+        </>
+    );
+}
