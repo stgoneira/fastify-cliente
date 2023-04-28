73ece3bb6e57e3d814150495725db41c1ad17a72
stgoneira <stgoneiran@gmail.com>
2023-04-27 20:21:41 -0400

    eliminar categoría

--- admin/components/categoria/listado.js
+++ admin/components/categoria/listado.js
@@ -22,6 +22,27 @@ export default function CategoriaListado() {
         cargarDatos();
     }, []);
 
+    const eliminar = async (categoria) => {
+        try {
+            //const baseUrl   = Configuracion.getBaseUrl();
+            const baseUrl   = 'http://localhost:3000';            
+            const url       = baseUrl + '/categoria?id='+categoria.id;
+            //const url       = baseUrl + '/categoria/'+categoria.id;
+
+            const respuesta = await fetch(url, {
+                method: 'DELETE'
+            });
+            if( !respuesta.ok ) throw new Error("No se pudo borrar la categoría!!!");
+            const resultado = await respuesta.json();
+            console.log("Categoría borrada de manera exitosa");
+
+            // actualizar el listado 
+            cargarDatos();
+        } catch( error ) {
+            console.error({error: error.message});
+        }
+    };
+
     return (
         <>
             <h2>Listado de Categorías</h2>
@@ -40,7 +61,10 @@ export default function CategoriaListado() {
                             <td>{categoria.id}</td>
                             <td>{categoria.nombre}</td>
                             <td>{categoria.descripcion}</td>
-                            <td></td>
+                            <td>
+                                <button>Editar</button>
+                                <button onClick={ () => eliminar(categoria) }>Eliminar</button>
+                            </td>
                         </tr>
                     ))}
                 </tbody>
